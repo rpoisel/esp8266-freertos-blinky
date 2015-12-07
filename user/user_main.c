ca7c7e7a841a50c8e1049dfff59b2cd85949542c
Rainer Poisel <rainer.poisel@gmail.com>
2015-12-07 23:29:39 +0000

    cleanup and wifi support

--- user/user_main.c
+++ user/user_main.c
@@ -5,11 +5,42 @@
 #include "freertos/task.h"
 
 #include "esp_common.h"
+#include "esp_wifi.h"
+
 #include "uart.h"
 
 #include "user_config.h"
 
-void helloworld(void *pvParameters)
+#define DEMO_AP_SSID "Demo"
+#define DEMO_AP_PASSWORD "passwd"
+
+static void user_setup(void);
+static void helloworld(void *pvParameters);
+
+void ICACHE_FLASH_ATTR user_init(void)
+{
+    user_setup();
+
+    xTaskCreate(helloworld, "hw", configMINIMAL_STACK_SIZE, NULL, 2, NULL);
+}
+
+static void user_setup(void)
+{
+    /* initialize UART */
+    uart_div_modify(UART0, UART_CLK_FREQ / (BIT_RATE_9600));
+
+    /* initialize WiFi */
+    wifi_set_opmode(STATION_MODE);
+    struct station_config * config = (struct station_config *)zalloc(sizeof(struct station_config));
+    sprintf(config->ssid, DEMO_AP_SSID);
+    sprintf(config->password, DEMO_AP_PASSWORD);
+    wifi_station_set_config(config);
+    free(config);
+    wifi_station_connect();
+
+}
+
+static void helloworld(void *pvParameters)
 {
     const portTickType xDelay = 1000 / portTICK_RATE_MS;
     for(;;)
@@ -19,9 +50,3 @@ void helloworld(void *pvParameters)
     }
 }
 
-//Init function 
-void ICACHE_FLASH_ATTR user_init(void)
-{
-    uart_div_modify(UART0, UART_CLK_FREQ / (BIT_RATE_9600));
-    xTaskCreate(helloworld, "hw", configMINIMAL_STACK_SIZE, NULL, 2, NULL);
-}
